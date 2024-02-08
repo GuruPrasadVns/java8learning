7a0e3c79b79fee1f61239622e3dbaa355683ea9e
prasag1 <guru.prasad@bsci.com>
2024-02-08 14:58:00 +0530

    BinaryTree notes and code committed

--- src/main/java/com/ds/binarytree/BinaryTree.java
+++ src/main/java/com/ds/binarytree/BinaryTree.java
@@ -5,225 +5,198 @@ import java.util.Queue;
 import java.util.Stack;
 
 class Node{
-	int data;
-	Node left;
-	Node right;
-	
-	public Node(int data) {
-		this.data = data;
-		this.left = null;
-		this.right = null;
-	}
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
-
 public class BinaryTree {
-	
-	Node root;
-	
-	
-	public BinaryTree() {
-		this.root = null;
-	}
-	
-	
-	public int height(Node root) {
-		if(root == null)
-			return 0;
-		
-		int leftSubTreeHeight = height(root.left);
-		int rightSubTreeHeight = height(root.right);
-		
-		return Math.max(leftSubTreeHeight, rightSubTreeHeight) + 1;
-	}
-	
-	public void levelOrder() {
-		int height = height(root);
-		
-		for(int i = 0; i <= height; i++)
-			printCurrentLevel(i, root);
-	}
+    Node root;
 
+    public BinaryTree(){
+        root = null;
+    }
 
-	private void printCurrentLevel(int level, Node root) {
-		if(root == null)
-			return;
-		
-		if(level == 1)
-			System.out.print(root.data + " ");
-		
-		else if(level > 1) {
-			printCurrentLevel(level - 1, root.left);
-			printCurrentLevel(level - 1, root.right);
-		}
-		
-	}
-	
-	public void inorder(Node root) {
-		if(root == null)
-			return;
-		
-		inorder(root.left);
-		System.out.print(root.data + " ");
-		inorder(root.right);
-	}
-	
-	public void preorder(Node root) {
-		if(root == null)
-			return;
-		
-		System.out.print(root.data + " ");
-		preorder(root.left);
-		preorder(root.right);
-	}
-	
-	public void postorder(Node root) {
-		if(root == null)
-			return;
-				
-		postorder(root.left);
-		postorder(root.right);
-		System.out.print(root.data + " ");
-		
-	}
-	
-	
-	public void inorderIterative() {
-		Stack<Node> stack = new Stack<>();
-		Node current = root;
-		
-		while(current != null || !stack.isEmpty()) {
-			
-			while(current != null) {
-				stack.push(current);
-				current = current.left;
-			}
-			
-			current = stack.pop();
-			System.out.print(current.data + " ");
-			current = current.right;
-		}
-	}
-	
-	
-	public int diameter(Node root) {
-		if(root == null)
-			return 0;
-		
-		int leftSubTreeHeight = height(root.left);
-		int rightSubTreeHeight = height(root.right);
-		
-		int leftSubTreeDiameter = diameter(root.left);
-		int rightSubTreeDiameter = diameter(root.right);
-		
-		return Math.max(leftSubTreeHeight + rightSubTreeHeight + 1, Math.max(leftSubTreeDiameter, rightSubTreeDiameter));
-	}
-	
-	public void insertionInLevelOrder(int key) {
-		
-		if(root == null) {
-			root = new Node(key);
-			return;
-		}
-		
-		Queue<Node> queue = new LinkedList<>();
-		queue.add(root);
-		Node current = null;
-		
-		while(!queue.isEmpty()) {
-			current = queue.poll();
-			
-			if(current.left == null) {
-				current.left = new Node(key);
-				break;
-			}else {
-				queue.add(current.left);
-			}
-			
-			if(current.right == null) {
-				current.right = new Node(key);
-				break;
-			}else {
-				queue.add(current.right);
-			}
-		}
-	}
-	
-	public void delete(int key) {
-		
-		if(root == null)
-			return;
-		
-		if(root.left == null && root.right == null) {
-				
-				if(root.data == key) {
-					root = null;
-				}
-				
-				return;
-		}
-		
-		Queue<Node> queue = new LinkedList<>();
-		Node current = root;
-		queue.add(current);
-		Node delNode = null;
-		Node temp = null;
-		
-		while(!queue.isEmpty()) {
-			temp = queue.poll();
-			
-			if(temp.data == key) {
-				delNode = temp;
-			}
-			
-			if(temp.left != null) {
-				queue.add(temp);
-			}
-			
-			if(temp.right != null) {
-				queue.add(temp.right);
-			}
-		}
-		
-		if(delNode != null) {
-			int x = temp.data;
-			deleteDeepest(root, temp);
-			delNode.data = x;
-		}
-	}
+    public int heightRec(Node root){
+        if(root == null) return 0;
 
+        int leftSubTreeHeight = heightRec(root.left);
+        int rightSubTreeHeight = heightRec(root.right);
 
-	private void deleteDeepest(Node root, Node deepestNode) {
-		Queue<Node> queue = new LinkedList<>();
-		queue.add(root);
-		Node temp = null;
-		
-		while(!queue.isEmpty()) {
-			temp = queue.poll();
-			
-			if(temp == deepestNode) {
-				temp = null;
-				return;
-			}
-			
-			if(temp.right != null) {
-				if(temp.right == deepestNode) {
-					temp.right = null;
-					return;
-				}else {
-					queue.add(temp.right);
-				}
-			}
-			
-			if(temp.left != null) {
-				if(temp.left == deepestNode) {
-					temp.left = null;
-					return;
-				}else {
-					queue.add(temp.left);
-				}
-			}
-		}
-		
-	}
+        return Math.max(leftSubTreeHeight, rightSubTreeHeight) + 1;
+    }
 
+    public void levelOrder(){
+        int height = heightRec(root);
+
+        for(int i = 1; i <= height; i++)
+            printCurrentLevel(root, i);
+    }
+
+    private void printCurrentLevel(Node root, int level) {
+        if(root == null) return;
+
+        if(level == 1){
+            System.out.print(root.data + " ");
+        }else if (level > 1){
+            printCurrentLevel(root.left, level - 1);
+            printCurrentLevel(root.right, level - 1);
+        }
+    }
+
+    public void inorder(Node root){
+        if(root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public void preorder(Node root){
+        if(root == null) return;
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public void postorder(Node root){
+        if(root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public void inorderIterative(){
+        if(root == null)
+            return;
+
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+
+        while(current != null || !stack.isEmpty()){
+
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            System.out.print(current.data + " ");
+            current = current.right;
+        }
+    }
+
+    public int diameter(Node root){
+        if(root == null) return 0;
+
+        int leftSubTreeHeight = heightRec(root.left);
+        int rightSubTreeHeight = heightRec(root.right);
+
+        int leftSubTreeDiameter = diameter(root.left);
+        int rightSubTreeDiameter = diameter(root.right);
+
+        return Math.max(leftSubTreeHeight + rightSubTreeHeight + 1, Math.max(leftSubTreeDiameter, rightSubTreeDiameter));
+    }
+
+    public void insertInLevelOrder(int key){
+        if(root == null){
+            root = new Node(key);
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        Node temp = null;
+
+        while(!queue.isEmpty()){
+            temp = queue.poll();
+
+            if(temp.left == null){
+                temp.left = new Node(key);
+                return;
+            }else{
+                queue.add(temp.left);
+            }
+
+            if(temp.right == null){
+                temp.right = new Node(key);
+                return;
+            }else{
+                queue.add(temp.right);
+            }
+        }
+    }
+
+    public void delete(int key){
+        if(root == null) return;
+        if(root.left == null && root.right == null){
+            if(root.data == key){
+                root = null;
+            }
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        Node delNode = null;
+        Node temp = null;
+
+        while(!queue.isEmpty()){
+            temp = queue.poll();
+
+            if(temp.data == key){
+                delNode = temp;
+            }
+
+            if(temp.left != null){
+                queue.add(temp.left);
+            }
+
+            if(temp.right != null){
+                queue.add(temp.right);
+            }
+        }
+
+        if(delNode != null){
+            int x = temp.data;
+            deleteDeepeset(root, temp);
+            delNode.data = x;
+        }
+    }
+
+    private void deleteDeepeset(Node root, Node deepestNode) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        Node temp = null;
+
+        while(!queue.isEmpty()){
+            temp = queue.poll();
+
+            if(temp == deepestNode){
+                temp = null;
+                return;
+            }
+
+            if(temp.right != null){
+                if(temp.right == deepestNode){
+                    temp.right = null;
+                    return;
+                }else{
+                    queue.add(temp.right);
+                }
+            }
+
+            if(temp.left != null){
+                if(temp.left == deepestNode){
+                    temp.left = null;
+                    return;
+                }else{
+                    queue.add(temp.left);
+                }
+            }
+        }
+    }
 }
--- src/test/java/com/ds/binarytree/BinaryTreeTest.java
+++ src/test/java/com/ds/binarytree/BinaryTreeTest.java
@@ -1,220 +1,295 @@
 package com.ds.binarytree;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class BinaryTreeTest {
-	
-//				1
-//				
-//		2                 3
-//		
-//	
-//	4
-	
-	@Test
-	public void basicBinaryTreeConstructionTest() {
-		BinaryTree tree = new BinaryTree();
-		tree.root = new Node(1);
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(3);
-		tree.root.left.left = new Node(4);
-		
-		assertEquals(3, tree.height(tree.root));
-		
-		System.out.println("\nlevleorder Traversal is :");
-		
-		tree.levelOrder();
-		
-		System.out.println("\nInorder Traversal is :");
-		
-		tree.inorder(tree.root);
-		
-		System.out.println("\nInorder Iterative Traversal is :");
-		
-		tree.inorderIterative();
-		
-		System.out.println("\nPreorder Traversal is :");
-		
-		tree.preorder(tree.root);
-		
-		System.out.println("\nPostorder Traversal is :");
-		
-		tree.postorder(tree.root);
-		
-		assertEquals(4, tree.diameter(tree.root));
-	}
-//						
-//												        25
-//												
-//										15					           50
-//										
-//								10				22              35			 70
-//								
-//						4 			12       18      24     31       44   66      90
-	
-	@Test
-	public void advancedBinaryTreeConstructionTest() {
-		BinaryTree tree = new BinaryTree();
-		
-		tree.root = new Node(25);
-		
-		tree.root.left = new Node(15);
-		tree.root.right = new Node(50);
-		
-		tree.root.left.left = new Node(10);
-		tree.root.left.right = new Node(22);
-		
-		tree.root.right.left = new Node(35);
-		tree.root.right.right = new Node(70);
-		
-		tree.root.left.left.left = new Node(4);
-		tree.root.left.left.right = new Node(12);
-		
-		tree.root.left.right.left = new Node(18);
-		tree.root.left.right.right = new Node(24);
-		
-		tree.root.right.left.left = new Node(31);
-		tree.root.right.left.right = new Node(44);
-		
-		tree.root.right.right.left = new Node(66);
-		tree.root.right.right.right = new Node(90);
-		
-		System.out.println("\nInorder Traversal is :");
-		
-		tree.inorder(tree.root);
-		
-		System.out.println("\nInorder Iterative Traversal is :");
-		
-		tree.inorderIterative();
-		
-		System.out.println("\nPreorder Traversal is :");
-		
-		tree.preorder(tree.root);
-		
-		System.out.println("\nPostorder Traversal is :");
-		
-		tree.postorder(tree.root);
-			
-		System.out.println("\nDiameter or width of tree is :");
-		
-		tree.diameter(tree.root);
-		
-		assertEquals(7, tree.diameter(tree.root));
-	}
-	
-//									1
-//								
-//							2				3
-//							
-//						4		5					8
-//						
-//							6		7						9
-//												
-//														10		11
-//														
-//															12
-	@Test
-	public void diameterSecondTest() {
-		
-		BinaryTree tree = new BinaryTree();
-		
-		tree.root = new Node(1);
-		
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(3);
-		
-		tree.root.left.left = new Node(4);
-		tree.root.left.right = new Node(5);
-		
-		tree.root.left.right.left = new Node(6);
-		tree.root.left.right.right = new Node(7);
-		
-		tree.root.right.right = new Node(8);
-		
-		tree.root.right.right.right = new Node(9);
-		
-		tree.root.right.right.right.left = new Node(10);
-		tree.root.right.right.right.right = new Node(11);
-		
-		tree.root.right.right.right.right.left = new Node(12);
-		
-		assertEquals(9, tree.diameter(tree.root));
-		
-	}
-	
-//							1
-//							
-//						2		3
-//						
-//					4		5
-//					
-//				6		7		 11
-//				
-//				     8        10     12
-//				     
-//				     	9           13
-				     	
-				     			
-	@Test
-	public void diameterThirdTest() {
-		BinaryTree tree = new BinaryTree();
-		
-		tree.root = new Node(1);
-		
-		tree.root.left = new Node(2);
-		tree.root.left = new Node(3);
-		
-		tree.root.left.left = new Node(4);
-		tree.root.left.right = new Node(5);
-		
-		tree.root.left.left.left = new Node(6);
-		tree.root.left.left.right = new Node(7);
-		
-		tree.root.left.left.right.left = new Node(8);
-		
-		tree.root.left.left.right.left.right = new Node(9);
-		
-		tree.root.left.right.right = new Node(11);
-		
-		tree.root.left.right.right.left = new Node(10);
-		tree.root.left.right.right.right = new Node(12);
-		
-		tree.root.left.right.right.right.left = new Node(13);
-		
-		assertEquals(9, tree.diameter(tree.root));
-	}
-	
-	@Test
-	public void insertionInLevelOrderTest() {
-		BinaryTree binaryTree = new BinaryTree();
-		
-		binaryTree.insertionInLevelOrder(1);
-		
-		binaryTree.insertionInLevelOrder(2);
-		binaryTree.insertionInLevelOrder(3);
-		
-		binaryTree.insertionInLevelOrder(4);
-		binaryTree.insertionInLevelOrder(5);
-		binaryTree.insertionInLevelOrder(6);
-		binaryTree.insertionInLevelOrder(7);
-		
-		binaryTree.insertionInLevelOrder(8);
-		binaryTree.insertionInLevelOrder(9);
-		binaryTree.insertionInLevelOrder(10);
-		binaryTree.insertionInLevelOrder(11);
-		
-		binaryTree.levelOrder();
-		
-		assertEquals(4, binaryTree.height(binaryTree.root));
-	}
-	
-	
-	@Test
-	public void deleteTest() {
-		BinaryTree binaryTree = new BinaryTree();
-		assertNull(binaryTree.root);
-	}
+import static org.junit.Assert.assertEquals;
 
+public class BinaryTreeTest {
+//                       1
+//
+//            2                     3
+//
+//    4               5       6             7
+
+    // Inorder : 4 2 5 1 6 3 7
+    // Preorder : 1 2 4 5 3 6 7
+    // Postorder : 4 5 2 6 7 3 1
+    @Test
+    public void simpleBinaryTreeTest(){
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = new Node(1);
+        binaryTree.root.left = new Node(2);
+        binaryTree.root.right = new Node(3);
+        binaryTree.root.left.left = new Node(4);
+        binaryTree.root.left.right = new Node(5);
+        binaryTree.root.right.left = new Node(6);
+        binaryTree.root.right.right = new Node(7);
+
+        assertEquals(3, binaryTree.heightRec(binaryTree.root));
+        assertEquals(5, binaryTree.diameter(binaryTree.root));
+        System.out.println("\nLevel order traversal is \n");
+        binaryTree.levelOrder();
+        System.out.println("\nIn order traversal is \n");
+        binaryTree.inorder(binaryTree.root);
+        System.out.println("\nPre order traversal is \n");
+        binaryTree.preorder(binaryTree.root);
+        System.out.println("\nPost order traversal is \n");
+        binaryTree.postorder(binaryTree.root);
+        System.out.println("\nIn order Iterative traversal is \n");
+        binaryTree.inorderIterative();
+
+        binaryTree.insertInLevelOrder(8);
+
+        System.out.println("\nAfter insertion level order traversal is \n");
+        binaryTree.levelOrder();
+    }
+
+    //                       1
+//
+//            2                     3
+//
+//    4               5       6             7
+
+    // delete(1) will result
+
+    //                       7
+//
+//            2                     3
+//
+//    4               5       6
+
+// Level order : 7 2 3 4 5 6
+// In order : 4 2 5 7 6 3
+// Preorder : 7 2 4 5 3 6
+    @Test
+    public void deleteKeyFirstTest(){
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = new Node(1);
+        binaryTree.root.left = new Node(2);
+        binaryTree.root.right = new Node(3);
+        binaryTree.root.left.left = new Node(4);
+        binaryTree.root.left.right = new Node(5);
+        binaryTree.root.right.left = new Node(6);
+        binaryTree.root.right.right = new Node(7);
+
+        binaryTree.delete(1);
+        System.out.println("\n After deleting key 1 the level order traversal is\n");
+        binaryTree.levelOrder();
+        System.out.println("\n After deleting key 1 the in order traversal is\n");
+        binaryTree.inorder(binaryTree.root);
+    }
+    //                       1
+//
+//            2                     3
+//
+//    4               5       6             7
+
+    // delete(2) will result
+
+    //                       1
+//
+//            7                   3
+//
+//    4               5       6
+
+// Level order : 1 7 3 4 5 6
+
+
+    @Test
+    public void deleteKeySecondTest(){
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = new Node(1);
+        binaryTree.root.left = new Node(2);
+        binaryTree.root.right = new Node(3);
+        binaryTree.root.left.left = new Node(4);
+        binaryTree.root.left.right = new Node(5);
+        binaryTree.root.right.left = new Node(6);
+        binaryTree.root.right.right = new Node(7);
+
+        binaryTree.delete(2);
+        System.out.println("\n After deleting key 2 the level order traversal is\n");
+        binaryTree.levelOrder();
+        System.out.println("\n After deleting key 2 the in order traversal is\n");
+        //binaryTree.inorder(binaryTree.root);
+    }
+
+    //				1
+//
+//		2                 3
+//
+//
+//	4
+
+    @Test
+    public void basicBinaryTreeConstructionTest() {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+
+        assertEquals(3, tree.heightRec(tree.root));
+
+        System.out.println("\nlevleorder Traversal is :");
+
+        tree.levelOrder();
+
+        System.out.println("\nInorder Traversal is :");
+
+        tree.inorder(tree.root);
+
+        System.out.println("\nInorder Iterative Traversal is :");
+
+        tree.inorderIterative();
+
+        System.out.println("\nPreorder Traversal is :");
+
+        tree.preorder(tree.root);
+
+        System.out.println("\nPostorder Traversal is :");
+
+        tree.postorder(tree.root);
+
+        assertEquals(4, tree.diameter(tree.root));
+    }
+//
+//												        25
+//
+//										15					           50
+//
+//								10				22              35			 70
+//
+//						4 			12       18      24     31       44   66      90
+
+    @Test
+    public void advancedBinaryTreeConstructionTest() {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(25);
+
+        tree.root.left = new Node(15);
+        tree.root.right = new Node(50);
+
+        tree.root.left.left = new Node(10);
+        tree.root.left.right = new Node(22);
+
+        tree.root.right.left = new Node(35);
+        tree.root.right.right = new Node(70);
+
+        tree.root.left.left.left = new Node(4);
+        tree.root.left.left.right = new Node(12);
+
+        tree.root.left.right.left = new Node(18);
+        tree.root.left.right.right = new Node(24);
+
+        tree.root.right.left.left = new Node(31);
+        tree.root.right.left.right = new Node(44);
+
+        tree.root.right.right.left = new Node(66);
+        tree.root.right.right.right = new Node(90);
+
+        System.out.println("\nInorder Traversal is :");
+
+        tree.inorder(tree.root);
+
+        System.out.println("\nInorder Iterative Traversal is :");
+
+        tree.inorderIterative();
+
+        System.out.println("\nPreorder Traversal is :");
+
+        tree.preorder(tree.root);
+
+        System.out.println("\nPostorder Traversal is :");
+
+        tree.postorder(tree.root);
+
+        System.out.println("\nDiameter or width of tree is :");
+
+        tree.diameter(tree.root);
+
+        assertEquals(7, tree.diameter(tree.root));
+    }
+
+    //									1
+//
+//							2				3
+//
+//						4		5					8
+//
+//							6		7						9
+//
+//														10		11
+//
+//															12
+    @Test
+    public void diameterSecondTest() {
+
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        tree.root.left.right.left = new Node(6);
+        tree.root.left.right.right = new Node(7);
+
+        tree.root.right.right = new Node(8);
+
+        tree.root.right.right.right = new Node(9);
+
+        tree.root.right.right.right.left = new Node(10);
+        tree.root.right.right.right.right = new Node(11);
+
+        tree.root.right.right.right.right.left = new Node(12);
+
+        assertEquals(9, tree.diameter(tree.root));
+
+    }
+
+//							1
+//
+//						2		3
+//
+//					4		5
+//
+//				6		7		 11
+//
+//				     8        10     12
+//
+//				     	9           13
+
+
+    @Test
+    public void diameterThirdTest() {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+
+        tree.root.left = new Node(2);
+        tree.root.left = new Node(3);
+
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        tree.root.left.left.left = new Node(6);
+        tree.root.left.left.right = new Node(7);
+
+        tree.root.left.left.right.left = new Node(8);
+
+        tree.root.left.left.right.left.right = new Node(9);
+
+        tree.root.left.right.right = new Node(11);
+
+        tree.root.left.right.right.left = new Node(10);
+        tree.root.left.right.right.right = new Node(12);
+
+        tree.root.left.right.right.right.left = new Node(13);
+
+        assertEquals(9, tree.diameter(tree.root));
+    }
 }
